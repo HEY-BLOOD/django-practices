d4179a4cbdd3cd117949b5b58579989c8aaae7e6
blood <hey-blood@foxmail.com>
2021-03-29 00:43:33 +0800

    更改  BookInstance列表视图，为 Author详细页面添加 Book模型的内联类

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -1,48 +1,71 @@
 from django.contrib import admin
-from .models import Author, Genre, Book, BookInstance
 
 # Register your models here.
-# admin.site.register(Book)
-# admin.site.register(Author)
-# admin.site.register(Genre)
-# admin.site.register(BookInstance)
+
+from .models import Author, Genre, Book, BookInstance
+"""Minimal registration of Models.
+admin.site.register(Book)
+admin.site.register(Author)
+admin.site.register(BookInstance)
+admin.site.register(Genre)
+"""
 
 
 # 内联类，TabularInline (水平布局 ) or StackedInline (垂直布局 ，就像默认布局)
 class BooksInstanceInline(admin.TabularInline):
+    """Defines format of inline book instance insertion (used in BookAdmin)"""
     model = BookInstance
     # 只显示“Add another Book instance”链接，而不显示备用的实例编辑表单
     extra = 0
 
 
-# Register the Admin classes for Author using the decorator
+class BooksInline(admin.TabularInline):
+    """Defines format of inline book insertion (used in AuthorAdmin)"""
+    model = Book
+    extra = 0
+
+
+class BookAdmin(admin.ModelAdmin):
+    """Administration object for Book models.
+    Defines:
+     - fields to be displayed in list view (list_display)
+     - adds inline addition of book instances in book view (inlines)
+    """
+    list_display = ('title', 'author', 'display_genre')
+    inlines = [BooksInstanceInline]
+
+
+admin.site.register(Book, BookAdmin)
+
+
 @admin.register(Author)
 class AuthorAdmin(admin.ModelAdmin):
+    """Administration object for Author models.
+    Defines:
+     - fields to be displayed in list view (list_display)
+     - orders fields in detail view (fields),
+       grouping the date fields horizontally
+     - adds inline addition of books in author view (inlines)
+    """
     list_display = ('last_name', 'first_name', 'date_of_birth',
                     'date_of_death')
     # 字段默认情况下垂直显示，但如果进一步将它们分组在元组中（如上述“日期”字段中所示），则会水平显示。
     # 注意：还可以使用exclude属性来声明要从表单中排除的属性列表（将显示模型中的所有其他属性）。
     fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+    inlines = [BooksInline]
 
 
-# Register the Admin classes for Book using the decorator
-@admin.register(Book)
-class BookAdmin(admin.ModelAdmin):
-    # 我们不能直接指定 list_display 中的 genre 字段， 因为它是一个ManyToManyField
-    # （Django可以防止这种情况，因为在这样做时会有大量的数据库访问“成本”）。
-    # 相反，我们将在 Book 模型中定义一个 display_genre 函数来获取信息作为一个字符串
-    list_display = ('title', 'author', 'display_genre')
-
-    # 通过声明 inlines，将内容中的 BookInstance信息添加到我们的 Book详细信息中
-    inlines = [BooksInstanceInline]
-
-
-# Register the Admin classes for BookInstance using the decorator
 @admin.register(BookInstance)
 class BookInstanceAdmin(admin.ModelAdmin):
+    """Administration object for BookInstance models.
+    Defines:
+     - fields to be displayed in list view (list_display)
+     - filters that will be displayed in sidebar (list_filter)
+     - grouping of fields into sections (fieldsets)
+    """
+    list_display = ('book', 'status', 'due_back', 'id')
     list_filter = ('status', 'due_back')
 
-    # 使用 fieldsets 属性添加“部分”以在详细信息表单中对相关的模型信息进行分组。
     fieldsets = (
         (None, {
             'fields': ('book', 'imprint', 'id')
