7dd69774885bc0e0eba25b790830ed934cfbf550
blood <hey-blood@foxmail.com>
2021-03-29 00:04:44 +0800

    添加列表过滤器

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -27,7 +27,7 @@ class BookAdmin(admin.ModelAdmin):
 # Register the Admin classes for BookInstance using the decorator
 @admin.register(BookInstance)
 class BookInstanceAdmin(admin.ModelAdmin):
-    pass
+    list_filter = ('status', 'due_back')
 
 
 # Register the Admin classes for Genre using the decorator
