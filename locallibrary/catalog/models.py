cbe539b89f141b3093928cc020ded6b7f0b2697a
blood <hey-blood@foxmail.com>
2021-03-29 21:39:41 +0800

    用户借阅书本：更改模型
    （添加借书者字段）

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.urls import reverse  # Used to generate URLs by reversing the URL patterns
 import uuid  # Required for unique book instances
+from django.contrib.auth.models import User
 
 
 # Create your models here.
@@ -89,6 +90,10 @@ class BookInstance(models.Model):
     book = models.ForeignKey('Book', on_delete=models.SET_NULL, null=True)
     imprint = models.CharField(max_length=200)
     due_back = models.DateField(null=True, blank=True)
+    borrower = models.ForeignKey(User,
+                                 on_delete=models.SET_NULL,
+                                 null=True,
+                                 blank=True)
 
     LOAN_STATUS = (
         ('m', 'Maintenance'),
@@ -102,6 +107,14 @@ class BookInstance(models.Model):
                               blank=True,
                               default='m',
                               help_text='Book availability')
+
+    @property
+    def is_overdue(self):
+        """Judge a particular book instance is overdue."""
+        from datetime import date
+        if self.due_back and date.today() > self.due_back:
+            return True
+        return False
 
     class Meta:
         ordering = ["due_back"]
